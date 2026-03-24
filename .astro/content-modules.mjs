65c408de05eb5bcfede26708f6b1414d6bea104e
Abdulaziz <abdulaziz@example.com>
2026-03-24 17:58:49 +0500

    some changes

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,9 @@
+
+export default new Map([
+["src/content/blog/biznes-strategiya-2024.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbiznes-strategiya-2024.mdx&astroContentModuleFlag=true")],
+["src/content/blog/innovatsiya-biznesda.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Finnovatsiya-biznesda.mdx&astroContentModuleFlag=true")],
+["src/content/blog/jamoa-boshqaruvi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fjamoa-boshqaruvi.mdx&astroContentModuleFlag=true")],
+["src/content/blog/moliyaviy-savodxonlik.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmoliyaviy-savodxonlik.mdx&astroContentModuleFlag=true")],
+["src/content/blog/raqamli-marketing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fraqamli-marketing.mdx&astroContentModuleFlag=true")],
+["src/content/blog/startup-g-oyalar.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fstartup-g-oyalar.mdx&astroContentModuleFlag=true")]]);
+		
