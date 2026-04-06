15d3a90b6d1e1ed8e9948b7629e3af4c02204575
Abdulaziz <abdulaziz@example.com>
2026-04-06 22:05:44 +0500

    add amazon

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/blog/airbnb-oddiy-goyadan-global-biznesgacha.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fairbnb-oddiy-goyadan-global-biznesgacha.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/airbnb-oddiy-goyadan-global-biznesgacha.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fairbnb-oddiy-goyadan-global-biznesgacha.mdx&astroContentModuleFlag=true")],
+["src/content/blog/amazon-startup-tarixi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Famazon-startup-tarixi.mdx&astroContentModuleFlag=true")]]);
 		
