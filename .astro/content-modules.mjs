0cc766055e757df6e213b1f233db48b49cd52e41
Abdulaziz <abdulaziz@example.com>
2026-04-25 20:35:01 +0500

    add starbucks article

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,5 +2,6 @@
 export default new Map([
 ["src/content/blog/airbnb-oddiy-goyadan-global-biznesgacha.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fairbnb-oddiy-goyadan-global-biznesgacha.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/amazon-startup-tarixi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Famazon-startup-tarixi.mdx&astroContentModuleFlag=true")],
+["src/content/blog/starbucks-qat-iyat-va-mijoz-tajribasi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fstarbucks-qat-iyat-va-mijoz-tajribasi.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/uber-tugma-orqali-taksi-revolutsiyasi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fuber-tugma-orqali-taksi-revolutsiyasi.mdx&astroContentModuleFlag=true")]]);
 		
